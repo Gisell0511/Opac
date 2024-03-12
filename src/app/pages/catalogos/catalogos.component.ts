8fce7ba5cb5439c0fb2d10d195205b6dd0419bab
JavierRuiz10 <javierruizorozco9@gmail.com>
2024-03-11 19:34:34 -0500

    CONSUMIR SERVICIOS DEL BACKEND

--- src/app/pages/catalogos/catalogos.component.ts
+++ src/app/pages/catalogos/catalogos.component.ts
@@ -1,28 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CatalogoService } from '../../services/catalogo.service';
 import { FormsModule } from '@angular/forms';
 import { PanelMenuModule } from 'primeng/panelmenu';
 import { CascadeSelectModule } from 'primeng/cascadeselect';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
 
 @Component({
   selector: 'app-catalogos',
   standalone: true,
-  imports: [FormsModule, PanelMenuModule, CascadeSelectModule, InputTextModule, ButtonModule],
+  imports: [FormsModule, PanelMenuModule, CascadeSelectModule, InputTextModule, ButtonModule, DropdownModule ],
   templateUrl: './catalogos.component.html',
   styleUrl: './catalogos.component.css'
 })
-export class CatalogosComponent {
+export class CatalogosComponent implements OnInit {
 
-  constructor(private readonly catalogoService: CatalogoService){}
+  public catalogos:any
 
-  catalogos: any[] = [
-    {
-      name: 'Corsalud',
-      id: '0'
-    }
-  ]
+  //constructor(private readonly catalogoService: CatalogoService){}
+  private catalogoService = inject(CatalogoService);
+
+  ngOnInit() {
+    this.buscarConsulta()
+  }
+
+  // catalogos: any[] = [
+  //   {
+  //     name: 'Corsalud',
+  //     id: '0'
+  //   }
+  // ]
+
   catalogoSeleccionado: string = ''
   tipoConsulta: any[] = [
     {
@@ -45,10 +54,11 @@ export class CatalogosComponent {
   tipoConsultaSeleccionado: string = ''
   consultaUsuario: string = ''
 
-  buscarConsulta(){
+  buscarConsulta() {
     console.log('buscar', this.consultaUsuario)
-    this.catalogoService.getCatalogos().subscribe((data: any) => {
-      console.log(data)
+    this.catalogoService.getCatalogos().subscribe((catalogos: any) => {
+      console.log({ catalogos })
+      this.catalogos = catalogos
     })
   }
 
