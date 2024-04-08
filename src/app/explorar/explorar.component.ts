60bfc4091352bdc095df0a446e9cd092f42e4c90
Gisell0511 <127627590+Gisell0511@users.noreply.github.com>
2024-04-08 14:30:13 -0500

    explorar

--- src/app/explorar/explorar.component.ts
+++ src/app/explorar/explorar.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-explorar',
   standalone: true,
-  imports: [ButtonModule,CardModule,InputTextModule],
+  imports: [ButtonModule,CardModule,InputTextModule,TableModule],
   templateUrl: './explorar.component.html',
   styleUrl: './explorar.component.css'
 })
