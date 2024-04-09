6f32a439ebb98ca77a51a43330e8232b2aa23775
Bornacelli <laurabornacelli02@gmail.com>
2024-04-09 00:20:31 -0500

    forms

--- src/app/consultas-avanzadas/routes/autor/autor.component.ts
+++ src/app/consultas-avanzadas/routes/autor/autor.component.ts
@@ -3,11 +3,12 @@ import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
 import { TableModule } from 'primeng/table';
+import { StepsModule } from 'primeng/steps';
 
 @Component({
   selector: 'app-autor',
   standalone: true,
-  imports: [ButtonModule,CardModule,InputTextModule,TableModule],
+  imports: [ButtonModule,CardModule,InputTextModule,TableModule,StepsModule],
   templateUrl: './autor.component.html',
   styleUrl: './autor.component.css'
 })
--- src/app/servicios-catalogos/routes/alertas-bibliograficas/alertas-bibliograficas.component.ts
+++ src/app/servicios-catalogos/routes/alertas-bibliograficas/alertas-bibliograficas.component.ts
@@ -3,11 +3,13 @@ import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
 import { TableModule } from 'primeng/table';
+import { GalleriaModule } from 'primeng/galleria';
+
 
 @Component({
   selector: 'app-alertas-bibliograficas',
   standalone: true,
-  imports: [ButtonModule,CardModule,InputTextModule,TableModule],
+  imports: [ButtonModule,CardModule,InputTextModule,TableModule,GalleriaModule],
   templateUrl: './alertas-bibliograficas.component.html',
   styleUrl: './alertas-bibliograficas.component.css'
 })
